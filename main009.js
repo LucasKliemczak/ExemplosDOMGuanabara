f71f45ce88c134aad77566ee78724866df422878
Lucas Amonati Khing Kliemczak <47717733+LucasKliemczak@users.noreply.github.com>
2023-08-19 21:01:08 -0300

    Add files via upload

---
+++ main009.js
@@ -0,0 +1,13 @@
+/*
+window.document.write('Ola mundo')
+
+window.document.write('<br>')
+window.document.write('<br>')
+
+window.document.write(window.document.getElementsByTagName('p')[2].innerHTML)
+
+var p1 = window.document.getElementsByTagName('p')[2].innerHTML
+
+p1.style.color = 'red'
+*/
+
